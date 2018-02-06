3272a2499cf7c80c742c22a9b78a28952f0b6838
keenanjacob <kjacob1@mail.csuchico.edu>
2018-02-05 17:31:37 -0800

    This program will tell you your biggest number! WOW!

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,16 +1,32 @@
-//Author:
+//Author: Keenan Jacob
 #include<iostream>
 
-using namespace std; //so we don't need the prefix std::
+using namespace std;
 
 int main()
 {
-  
-  cout<<"Please enter a whole number:\n";
-  
-  cout<<"Please enter another whole number:\n";
+ int number1;
+ int number2;
+ int answer;
 
-  cout<<"Of those two numbers, the biggest is: ";
+  cout<<"Please enter a whole number:\n";
+  cin>>number1;
+ 
+  cout<<"Please enter another whole number:\n";
+  cin>>number2;
+
+  if(number1>number2)
+  {
+  answer=number1;
+  }
+  else
+  {
+  answer=number2;
+  }
+
+
+  cout<<"Of those two numbers, the biggest is: "<<answer;
+
   cout<<endl<<"Thank you for playing.\n";
 
   return 0;
